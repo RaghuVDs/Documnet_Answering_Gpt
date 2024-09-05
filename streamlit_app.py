8a804bfa39be709d0e1637267f07a803145c8652
Raghuveera Narasimha <147996831+RaghuVDs@users.noreply.github.com>
2024-09-05 12:06:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,9 @@ import streamlit as st
 from lab1 import lab1 
 from lab2 import lab2  
 
-pages = [
+pg = st.navigation([
     st.Page(lab1, title="Lab 1"),
     st.Page(lab2, title="Lab 2")
-]
-
-pg = st.navigation(pages, position="sidebar")
+], position="sidebar")
 
 pg.run()
