3153917c564c8d3e17812ca8b728fb2cfcddcb34
Raghuveera Narasimha <147996831+RaghuVDs@users.noreply.github.com>
2024-10-09 17:52:21 +0000

    lab5

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ from lab01 import lab1
 from lab02 import lab2 
 from lab03 import lab3
 from lab04 import lab4
+from lab05 import lab5
+
 
 
 with st.sidebar:
-    selected_page = st.radio("Select a page", ["Lab 1", "Lab 2", "Lab 3","Lab 4"])
+    selected_page = st.radio("Select a page", ["Lab 1", "Lab 2", "Lab 3","Lab 4","Lab 5"])
 
 # Display the selected page
 if selected_page == "Lab 1":
@@ -17,4 +19,6 @@ elif selected_page == "Lab 3":
     lab3()
 elif selected_page == "Lab 4":
     lab4()
+elif selected_page == "Lab 5":
+    lab5()
     
